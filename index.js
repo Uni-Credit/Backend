620fcd29dbeecc43e3891ff30de967cb49533625
Emily Salum <65673894+emilymarquessalum@users.noreply.github.com>
2023-08-04 14:55:55 -0300

    base mvc template

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser"); 
+const morgan = require("morgan");
+const cors = require("cors"); 
+const app = express();
+ 
+// * Cors
+app.use(cors());
+
+// * Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("short"));
+
+
+app.use("*", (req, res) => {
+  res.send("Route not found");
+});
+
+let PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
