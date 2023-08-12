e9645e3a1081bdd061a99598d560c2fbc39911b6
Emily Salum <65673894+emilymarquessalum@users.noreply.github.com>
2023-08-12 16:05:34 -0300

    auth module

--- index.js
+++ index.js
@@ -20,3 +20,5 @@ app.use("*", (req, res) => {
 let PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+
+app.use('auth/', /* Authentication Route */);
