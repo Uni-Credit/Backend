91d5a53d04520d7a5b8de46dd71a9f9fe58e17d8
Emanuel Silva <emanuel_silva2004@hotmail.com>
2023-10-31 19:34:59 -0300

    Validação de login

--- controllers/authentication_controller.js
+++ controllers/authentication_controller.js
@@ -7,6 +7,19 @@ class Authenticationcontroller {
     // #TODO
     static loginUser(req, res) {
 
+        console.log(req.body);
+
+        if (req.body.matricula == null || req.body.password == null){
+
+            res.status(400).send({"message":"Matrícula ou senha vazios"});
+            return;
+        }
+
+        if ([5, 6].includes(req.body.matricula.length) == null || req.body.password.length < 6){
+            res.status(400).send({"message":"Matrícula ou senha incorretos"});
+            return;
+        }
+
         UserModel.loginUser(req.body)
         .then((result)=>{
             res.status(200).send(result);
